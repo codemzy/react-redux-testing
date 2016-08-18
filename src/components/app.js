29be648bb1c431d1bb7c7e566b7ed3c25a070102
Emma J <codemzy@gmail.com>
2016-08-18 09:29:15 +0000

    Added CommentList component and initial test

--- src/components/app.js
+++ src/components/app.js
@@ -2,12 +2,14 @@ import React from 'react';
 
 // components
 import CommentBox from './CommentBox';
+import CommentList from './CommentList';
 
 export default class App extends React.Component {
   render() {
     return (
       <div>
         <CommentBox />
+        <CommentList />
       </div>
     );
   }
